93b6a8bbab60a41ff6d5d74058e0cfbab271a194
sungmingo <sungmin360@nate.com>
2025-04-10 19:24:39 +0900

    new

--- src/main/java/com/example/project_sample/config/SecurityConfiguration.java
+++ src/main/java/com/example/project_sample/config/SecurityConfiguration.java
@@ -1,67 +1,38 @@
-        package com.example.project_sample.config;
+@Configuration
+public class SecurityConfiguration {
 
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-        import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-        import org.springframework.security.crypto.password.PasswordEncoder;
+        @Autowired
+        private UserService service;
 
-        import com.example.project_sample.service.UserService;
-
-        import lombok.RequiredArgsConstructor;
-
-        @EnableWebSecurity
-        @Configuration
-        @RequiredArgsConstructor
-        public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        
-                private final UserService userService;
-
-
-
-                
-                @Override
-                protected void configure(HttpSecurity http ) throws Exception {
-                                        
-                        http
-                                        .cors().disable() // cors방지
-                                        .csrf().disable() // csrf방지
-                                        .formLogin().disable() // 기본 로그인 페이지 없애기
-                                        .headers().frameOptions().disable();
-
-                        http
-                        .csrf().disable()
-                        .authorizeRequests()
-                        .antMatchers("/manager/man_login_Form").authenticated()
-                        .antMatchers("/manager/**").hasAuthority("관리자")
+        @Bean //빈을 DelegatingFilterProxy와 FilterChainProxy에 연결하여 모든 웹 요청에 보안 필터를 자동 적용
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .cors().disable()
+                                .csrf().disable() // < -- 보안 취약점 개발 환경에서 사용
+                                .headers().frameOptions().disable() // < -- 보안 취약점 개발 환경에서 사용
+                                .and()
+                                .authorizeRequests()
+                                .antMatchers("/manager/man_login_Form").authenticated()
+                                .antMatchers("/manager/**").hasAuthority("관리자")
                                 .anyRequest().permitAll()
                                 .and()
-                        .formLogin()
+                                .formLogin()
                                 .loginPage("/manager/man_login_Form")
                                 .loginProcessingUrl("/manager/man_login")
                                 .defaultSuccessUrl("/manager/main")
                                 .failureUrl("/manager/man_login_Form")
                                 .permitAll()
                                 .and()
-                        .logout()
-                                .permitAll();
-                }
+                                .logout()
+                                .permitAll()
+                                .and()
+                                .userDetailsService(service);
 
-
-                
-
-
-                @Override
-                public void configure(AuthenticationManagerBuilder auth) throws Exception {
-                        auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
-                }
-
-                @Bean
-                public PasswordEncoder encodePWD() {
-                        return new BCryptPasswordEncoder(); // 이 객체를 스프링이 관리하게 됨. 필요할 때마다 가져가서 쓰면 된다.
-                }
-
+                return http.build();
         }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+}
--- src/main/java/com/example/project_sample/config/ThymeleafConfig.java
+++ src/main/java/com/example/project_sample/config/ThymeleafConfig.java
@@ -1,33 +1,49 @@
 package com.example.project_sample.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.templatemode.TemplateMode;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import com.example.project_sample.service.UserService;
 
 @Configuration
-public class ThymeleafConfig {
+public class SecurityConfiguration {
 
-    @Bean
-    public TemplateEngine htmlTemplateEngine() {
-        TemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.addTemplateResolver(springResourceTemplateResolver());
+        @Autowired
+        private UserService service;
 
-        return templateEngine;
-    }
+        @Bean //빈을 DelegatingFilterProxy와 FilterChainProxy에 연결하여 모든 웹 요청에 보안 필터를 자동 적용
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .cors().disable()
+                                .csrf().disable() // < -- 보안 취약점 개발 환경에서 사용
+                                .headers().frameOptions().disable() // < -- 보안 취약점 개발 환경에서 사용
+                                .and()
+                                .authorizeRequests()
+                                .antMatchers("/manager/man_login_Form").authenticated()
+                                .antMatchers("/manager/**").hasAuthority("관리자")
+                                .anyRequest().permitAll()
+                                .and()
+                                .formLogin()
+                                .loginPage("/manager/man_login_Form")
+                                .loginProcessingUrl("/manager/man_login")
+                                .defaultSuccessUrl("/manager/main")
+                                .failureUrl("/manager/man_login_Form")
+                                .permitAll()
+                                .and()
+                                .logout()
+                                .permitAll()
+                                .and()
+                                .userDetailsService(service);
 
-    @Bean
-    public SpringResourceTemplateResolver springResourceTemplateResolver() {
-        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
-        springResourceTemplateResolver.setOrder(1);
-        springResourceTemplateResolver.setPrefix("classpath:templates/");
-        springResourceTemplateResolver.setSuffix(".html");
-        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
-        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
-        springResourceTemplateResolver.setCacheable(false);
+                return http.build();
+        }
 
-        return springResourceTemplateResolver;
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 }
